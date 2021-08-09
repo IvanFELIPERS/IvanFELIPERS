91f826050ca13c45858f5d9d8db484dc4b5f0555
IvanFELIPERS <ifrodriguez34@misena.edu.co>
2021-08-08 23:17:52 -0500

    Se agrega actividad con la conexion a diferentes bases de datos

--- Petoys/Petoys/Petoys/settings.py
+++ Petoys/Petoys/Petoys/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import Petoys.db as db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,13 +74,7 @@ WSGI_APPLICATION = 'Petoys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = db.POSTGRESQL
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
